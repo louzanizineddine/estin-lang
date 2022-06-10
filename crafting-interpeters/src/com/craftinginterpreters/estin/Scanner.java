57f4c457e2328d5369520d15982f6ae5657576f4
zineddine LOUZANI <31287591+louzanizineddine@users.noreply.github.com>
2022-06-10 16:09:56 +0200

    Add files via upload

---
+++ crafting-interpeters/src/com/craftinginterpreters/estin/Scanner.java
@@ -0,0 +1,66 @@
+package com.craftinginterpreters.estin;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.craftinginterpreters.estin.TokenType.*;
+
+class Scanner {
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+    private int start = 0;
+    private int current= 0;
+    private int line = 0;
+    
+    Scanner(String source) {
+        this.source = source;
+    }
+
+    List<Token> scanTokens() {
+        while (!isAnEnd()) {
+            start = current;
+            scanToken();
+        }
+        tokens.add(new Token(EOF, "", null, line));
+        return tokens;
+    }
+
+    private void scanToken() {
+        char c = advance();
+        switch (c) {
+            case '(': addToken(LEFT_PAREN); break;
+            case ')': addToken(RIGHT_PAREN); break;
+            case '{': addToken(LEFT_BRACE); break;
+            case '}': addToken(RIGHT_BRACE); break;
+            case ',': addToken(COMMA); break;
+            case '.': addToken(DOT); break;
+            case '-': addToken(MINUS); break;
+            case '+': addToken(PLUS); break;
+            case ';': addToken(SEMICOLON); break;
+            case '*': addToken(STAR); break;
+
+            default:
+                Estin.error(line, "unexpected char");
+                break;
+        }
+    }
+
+
+    private boolean isAnEnd() {
+        return current >= source.length();
+    }
+
+    private char charAdvance() {
+        return source.charAt(current++);
+    }
+
+    private void addToken(TokenType type) {
+        addToken(type , null);
+    }
+
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+}
