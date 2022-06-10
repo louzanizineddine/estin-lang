1f005e6084dae6dd6fda41e051bacee88f954723
louzanizineddine <louzanizineddine@gmail.com>
2022-06-10 18:20:33 +0100

    dealed with some cases in Operators

--- crafting-interpeters/src/com/craftinginterpreters/estin/Scanner.java
+++ crafting-interpeters/src/com/craftinginterpreters/estin/Scanner.java
@@ -39,13 +39,52 @@ class Scanner {
             case '+': addToken(PLUS); break;
             case ';': addToken(SEMICOLON); break;
             case '*': addToken(STAR); break;
-
+            case '!':
+                addToken(match('=') ? BANG_EQUAL : BANG);
+                break;
+            case '=':
+                addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+                break;
+            case '<':
+                addToken(match('=') ? LESS_EQUAL : LESS);
+                break;
+            case '>':
+                addToken(match('=') ? GREATER_EQUAL : GREATER);
+                break;
+            case '/':
+                if (match('/')) {
+                    // A comment that goes until the end of the line
+                    while (peek() != '\n' && !isAnEnd()) advance();
+                    else {
+                        addToken(SLASH);
+                    }
+                }
+            case ' ':
+            case '\r':
+            case '\t':
+                // Ignore white spaces
+                break;
+            case '\n':
+                line++;
+                break;
             default:
                 Estin.error(line, "unexpected char");
                 break;
         }
     }
 
+    private boolean match(char expected) {
+        if (isAnEnd()) return false;
+        if(source.charAt(current) != expected) return false;
+        current ++;
+        return true;
+    }
+
+    // do not consume the current char but lookahead
+    private char advance() {
+        if (isAnEnd()) return '\0';
+        return source.charAt(current);
+    }
 
     private boolean isAnEnd() {
         return current >= source.length();
