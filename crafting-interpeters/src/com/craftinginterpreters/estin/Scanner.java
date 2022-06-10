56cefe2b16c8003545523df884f7e8efd5be2400
louzanizineddine <louzanizineddine@gmail.com>
2022-06-10 18:48:29 +0100

    Number Literals cases

--- crafting-interpeters/src/com/craftinginterpreters/estin/Scanner.java
+++ crafting-interpeters/src/com/craftinginterpreters/estin/Scanner.java
@@ -69,9 +69,28 @@ class Scanner {
                 break;
             case '"': string(); break;
             default:
-                Estin.error(line, "unexpected char");
+                if(isDigit(c)){
+                    number();
+                }else {
+                    Estin.error(line, "unexpected char");
+                }
                 break;
         }
+    }
+
+    private void number() {
+        while (isDigit(peek())) advance();
+
+        // Look for a fractional part.
+        if (peek() == '.' && isDigit(peekNext())) {
+            // Consume the "."
+            advance();
+
+            while (isDigit(peek())) advance();
+        }
+
+        addToken(NUMBER,
+                Double.parseDouble(source.substring(start, current)));
     }
 
     private void string() {
@@ -102,6 +121,16 @@ class Scanner {
         if (isAtEnd()) return '\0';
         return source.charAt(current);
     }
+
+    private char peekNext() {
+        if (current + 1 >= source.length()) return '\0';
+        return source.charAt(current + 1);
+    }
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+
 
 
     private boolean isAtEnd() {
